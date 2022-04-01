4b8ebed699ee1553019488a8108f355f0d28920f
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-04-01 01:17:49 +0000

    modify validation message

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,23 +5,23 @@
  */
 
 $(document).ready(() => {
-
+  //function that loops through the database which is an array of objects, callbacks the createTweetElement function then pushes the newly generated data into the database.
   const renderTweets = (data) => {
-  
+
     for (const userData of data) {
       const $eachTweet = createTweetElement(userData);
-      $('#tweets-container').prepend($eachTweet); 
+      $('#tweets-container').prepend($eachTweet);
     }
   };
-
-  const escape = function (str) {
+  //escape sequence that changes the <> symbols to something else.
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
-    return div.innerHTML; //changes the <> symbols to something else 
+    return div.innerHTML;
   };
-
+  //function that creates a new article tag of tweet data given the user input data
   const createTweetElement = (tweetData) => {
-  const $tweet = $(`<article name="tweet" class="tweet">
+    const $tweet = $(`<article name="tweet" class="tweet">
   <header>
     <span class="icon-format"><img src="${escape(tweetData.user.avatars)}" alt="avatar-icon" height="52px" width="52px">
       <span>${escape(tweetData.user.name)}</span>
@@ -39,32 +39,35 @@ $(document).ready(() => {
       </div>
     </footer>
   </article>`);
-  return $tweet;
+    return $tweet;
   };
-
+  //submit event listener that is triggered with user interaction
   $("#new-tweet-form").submit(function(event) {
     event.preventDefault();
     const tweetTextVal = $("#tweet-text").val();
-    
+    //conditional checks to trigger error message
     if (tweetTextVal === "" || tweetTextVal === null) {
-      return $(".validation").text("Let your voice be heard!").slideDown();
+      return $(".validation").text("⚠️Let your voice be heard!⚠️").slideDown();
     }
     if (tweetTextVal.length > 140) {
-      return $(".validation").text("Tweet too long, try again!").slideDown();
+      return $(".validation").text("⚠️Tweet too long, try again!⚠️").slideDown();
     }
-    $(".validation").hide();
+    $(".validation").hide();//hides the error if conditionals return false.
     const data = $(this).serialize();
+    //makes ajax POST request to the /tweets route then invokes loadTweets with the data
     $.ajax({
       type: "POST",
       url: "/tweets",
       data: data
     }).then(() => {
       loadTweets();
+      //after loadTweets is invoked clear textarea
       $("#tweet-text").val("");
+      //resets the counter back to 140 after post request is made
       $("#tweet-text").parent().find(".counter").text(140);
     });
   });
-
+  //function that makes an ajax GET request to /tweets that invokes renderTweets and returns the database of tweets
   const loadTweets = () => {
     $.ajax({
       type: "GET",
@@ -72,9 +75,8 @@ $(document).ready(() => {
     }).then((data) => {
       $("#tweets-container").empty();
       renderTweets(data);
-    })
+    });
   };
   loadTweets();
-
 });
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   const newTweets = $("#tweet-text");
-  newTweets.on("input", function() {
+  newTweets.on("input", function() { //event handler for input
     const characterCount = $(this).val().length;
     const outputLength = 140 - characterCount;
     const counterSelector = $(this).parent().find(".counter").text(outputLength);//.text = replacing the text within the tag
     
-    if (outputLength <= 0) {
-      counterSelector.css({color: "#FF0000"})
-    } 
-    if (outputLength > 0) {
-      counterSelector.css({color: "#000000"})
+    if (outputLength < 0) {
+      counterSelector.css({color: "#FF0000"});
+    }
+    if (outputLength >= 0) {
+      counterSelector.css({color: "#000000"});
     }
   });
 });
